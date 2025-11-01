4985c3799cd51f9453d9fd0b4b08729fd9a68cab
manvirsingh14 <manvirtaneja07@gmail.com>
2025-11-02 00:18:41 +0530

    added engine path

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from "@manvirsingh7/payzap-database/generated/prisma/client";
+import path from "path";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma ?? new PrismaClient();
+const enginePath = path.resolve(
+  "node_modules/@manvirsingh7/payzap-database/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node"
+);
 
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+process.env.PRISMA_QUERY_ENGINE_LIBRARY = enginePath;
+
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log: ["query", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
