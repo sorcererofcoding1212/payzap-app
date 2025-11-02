c5a390863587278391bdac20a5d4a99886eef093
manvirsingh14 <manvirtaneja07@gmail.com>
2025-11-02 22:40:05 +0530

    updated env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-export const BANK_URL = "http://localhost:4000";
-export const SOCKET_URL = "http://localhost:8080";
+export const BANK_URL = process.env.BANK_URL
+export const SOCKET_URL = process.env.SOCKET_URL
